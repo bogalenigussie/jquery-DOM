689341d74e7636fb96f9a8a0922a19d52969efec
sonice2004 <solomon.tolosa@gmail.com>
2022-01-12 20:02:24 -0500

    Update and rename jq1.js to main.js
    Overall, Amazing work! I am impressed.  A couple of I made to clean it up further. 
    1) moved the html/div out side the event binding. That html1/redDiv will be created once when the page is loaded and that variable can be reused on every click. If you have it inside the click it will be regenerated everytime you click (reduced the performance let's say if the click is done 1000 times).
    2) just formatting to make the core more readable

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+    var redDiv= 
+            "<div><button style=background:red;width:80px;height:20px></button>\
+            <Button id=hide_btn style=background:yellow;border-radius:50%;display:inline>X</button>\
+            </div>"   
+            $(".red1").append(html1);
+    
+    var greenDiv=
+        "<div><button style=background:green;width:80px;height:20px></button>\
+        <Button id=hide_btn style=background:lime;border-radius:50%;display:inline>X</button>\
+        </div>";
+    
+    //event listeners 
+    $("#btn_r").click(function(){ 
+            $(".red1").append(redDiv);
+    });
+    
+   $("#btn_g").click(function(){ 
+        $(".green1").append(greenDiv);
+   });
+    
+   $(".red1").on("click","#hide_btn",function() {
+        console.log("delete button clicked")
+        $(this).parent().hide();
+    });  
+      
+    $(".green1").on("click","#hide_btn",function() { 
+        console.log("delete button clicked")
+        $(this).parent().hide();
+     }); 
+    
+  });  
